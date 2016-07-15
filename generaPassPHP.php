ce8982e803f0939d1188209e16e73c75cf0c0bd5
vstefano <stefanovigotti52@gmail.com>
2016-07-15 18:02:51 +0200

    creato genera pass base

--- generaPassPHP.php
+++ generaPassPHP.php
@@ -1,54 +1,36 @@
 
-<?php
-echo "GENERA LA TUA PASSWORD";
+<h2>GENERA LA TUA PASSWORD</h2>
 
-//ACQUISIZIONE NOME SITO
-
-if(strlen($_GET['name']) > 0){
-    ?>
-    </br> Il sito su cui userai la password è:  <?php echo $_GET['name'] ?>  </br> </br>
-    <?php
-}else{
-    ?>
-    <form action="generaPassPHP.php" method="GET">
-        <p>Inserisci il nome del sito sul quale userai la password: <input type="text" name="name" value="" /></p>
-        <p><input type="submit"></p>
-    </form>
-    <?php
-}//Fine else
-?>
+<form action="generaPassPHP.php" method="GET">
+    <p>Inserisci il nome sito: <input type="text" name="name" value="<?php echo $_GET['name'] ?>" /></p>
+    <p><input type="submit"></p>
+</form>
+</br> <p>Sito:  <?php echo $_GET['name'] ?> </p> </br> </br>
 
 <!--ACQUISIZIONE LUNGHEZZA PASSWORD-->
 
-<?php
-if(strlen($_GET['dim']) >0){
-    ?>
-    </br><p> hai scelto una dimensione di: <?php echo $_GET['dim']?> per la password</p>
-    <?php
-}else{
-    ?>
-    <form action="generaPassPHP.php" method="GET">
-        <p>Inserisci la dimensione desiderata per la password: <input type="text" name="dim" value="" /></p>
-        <p><input type="submit"></p>
-    </form>
-    <?php
-}//Fine else
-?>
+<form action="generaPassPHP.php" method="GET">
+    <p>Dimensione della password: <input type="text" name="dim" value="<?php echo $_GET['dim'] ?>" /></p>
+    <input type="hidden" name="name" value="<?php echo $_GET['name'] ?>"  />
+    <p><input type="submit"></p>
+</form>
+</br> <p>Dimensione password:  <?php echo $_GET['dim'] ?> </p> </br> </br>
 
 
 <?php
-//CREAZIONE PASSWORD
 
 $pass="";
 $dimensione=$_GET['dim'];
-echo $dimensione;
+echo "Dimensione: ".$dimensione."</br>";
 
-
-
-for($i=0;i<$dimensione/2;++$i){
-    pass.rand(0,9);
+for($i=0;$i<$dimensione/2;++$i){
+    $pass=$pass . rand(0,9);
 }
-echo "attualmente la tua password è: ".$pass ;
+echo "Password con numeri: ". $pass."</br>";
 
-
+for($i=0;$i<$dimensione/2;++$i){
+   echo chr(rand(65,90))." </br>";
+}
+//echo " test : ".chr(69)."</br>";
+echo "Password completa: ". $pass."</br>";
 ?>
